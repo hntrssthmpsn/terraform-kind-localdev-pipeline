187b770655a111b6b84ef21e965561ad86b7c1e2
Cassandra Gregg <hntrssthmpsn@gmail.com>
2023-10-20 14:31:58 -0700

    Added toggle for docker registry

--- docker-registry.tf
+++ docker-registry.tf
@@ -1,11 +1,13 @@
 resource "docker_image" "kind_registry" {
-  name = "registry:2.8"
+  count = var.docker_registry_enabled ? 1 : 0
+  name  = "registry:${var.docker_registry_image_tag}"
 }
 
 
 resource "docker_container" "kind_registry" {
+  count    = var.docker_registry_enabled ? 1 : 0
   name     = "kind-registry"
-  image    = docker_image.kind_registry.image_id
+  image    = docker_image.kind_registry.0.image_id
   must_run = true
   restart  = "always"
   networks_advanced {
